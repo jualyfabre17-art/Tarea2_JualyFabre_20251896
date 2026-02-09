09f583b99bdb7c00446c3f6aa93eba65dc25ef9e
jualyfabre17-art <jualyfabre17@gmail.com>
2026-02-08 20:34:04 -0400

    Add files via upload

---
+++ Tarea2 Jualy Fabre/Program.cs
@@ -0,0 +1,14 @@
+﻿//Tarea 2 - Jualy Fabre 2025-1896
+
+Console.WriteLine("Ingrese el numero que quiera saber si par o impar:\n");
+int number = int.Parse(Console.ReadLine());
+
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("El numero " + number + " es PAR");
+}
+else
+{
+    Console.WriteLine("El numero " + number + " es IMPAR");
+}
